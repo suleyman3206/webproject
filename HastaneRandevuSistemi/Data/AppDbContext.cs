ded0475c0339762b6cf8ba32236789f9e321375b
tberkk <93779700+tberkk@users.noreply.github.com>
2024-01-01 17:56:15 +0300

    web project

---
+++ HastaneRandevuSistemi/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HastaneRandevuSistemi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Hastane> HastaneTable { get; set; }
+        public DbSet<Poliklinik> PoliklinikTable { get; set; }
+        public DbSet<Doktor> DoktorTable { get; set; }
+        public DbSet<Randevu> RandevuTable { get; set; }
+        public DbSet<User> UserTable { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-ES69NQK; database=HastaneDB; Integrated Security=false; TrustServerCertificate=True; Trusted_Connection=True; MultipleActiveResultSets=true;");
+        }
+    }
+}
